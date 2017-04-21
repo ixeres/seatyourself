b65c7833efb33bdacd0ec7ec7a3b27a2a2964217
Zachary Viggiani <zach@bigmojo.net>
2017-04-21 16:33:07 -0400

    Users pages all working now

--- db/migrate/20170421180936_create_uers.rb
+++
@@ -1,9 +0,0 @@
-class CreateUser < ActiveRecord::Migration[5.0]
-  def change
-    create_table :user do |t|
-      t.string :username
-      t.string :email
-      t.string :password
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421155833) do
+ActiveRecord::Schema.define(version: 20170421183804) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20170421155833) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
