be0a8d9eb593b875f8cfa39a220116e53233a460
Zachary Viggiani <zach@bigmojo.net>
2017-04-21 15:45:54 -0400

    user controllers setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,50 @@
 class UsersController < ApplicationController
+  def index
+    @users = Users.all
+  end
+
+  def show
+    @user = Users.find(params[:id])
+  end
   def new
+    @user = Users.new
   end
 
   def edit
+    @user = Users.find(params[:id])
   end
 
-  def update
+  def create
+    @user = Users.new(user_params)
+
+    if @users.save
+      redirect_to  user_url
+    else
+      render :new
+    end
   end
 
-  def delete
+end
+
+def update
+  @user = User.find(params[:id])
+
+  if @user.update_attributes(user_params)
+    redirect_to user_url(@user)
+  else
+    render :edit
   end
+end
+
+def destroy
+  @user = user.find(params[:id])
+  @user.destroy
+  redirect_to users_url
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :description, :address, :menu, :guests, :image)
+end
 end
