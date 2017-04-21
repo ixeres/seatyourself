743b758e1b7ff6f74b2bfd8337183676513797ca
Zachary Viggiani <zach@bigmojo.net>
2017-04-21 14:44:10 -0400

    created models

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/username'
 
-  get 'user/email'
+  resources :user
 
-  get 'user/password'
-
-resources :restaurants
+  resources :restaurants
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
