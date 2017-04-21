b38a27de5129e2405f13cad8cf9354c9da311a43
Devin Leduc <ixeres@gmail.com>
2017-04-21 10:57:23 -0400

    Added controllers for Restaurant model. Moving into branch for further work.

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,45 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_url(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_url
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :address, :menu, :restaurant_id, :guests)
+  end
+end
