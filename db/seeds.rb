9b930b52e4824eb48541fb2c286bd23036647f4b
Alex <alex.mahinko@gmail.com>
2017-04-24 16:57:01 -0400

    Added user seed, fixed bug with users/show

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ image: "http://images.complex.com/complex/image/upload/c_limit,w_680/fl_lossy,pg
 
 Restaurant.create(
 name: "Donnie T's Family Funtime Fantasy Feistaurant",
-description: "Family-style mexican resteraunt and arcade. Come for the animatronics, stay because we took your passport.",
+description: "Family-style mexican restaurant and arcade. Come for the animatronics, stay because we took your passport.",
 address: "555 Tinkledown Boulevard",
 menu: "Literally cheetos in a torilla shell.",
 guests: 45,
