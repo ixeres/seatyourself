4ae61baa73ff85f57e865b33883ec4ea7eec64ad
Devin Leduc <ixeres@gmail.com>
2017-04-23 23:21:07 -0400

    Added user validations, sessions. Started refining bookings model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
    has_secure_password
+   validates :email, :password, presence: true
+   validates :email, :name, uniqueness: true
+   validates :password, confirmation: true
+   validates :password_confirmation, presence: true
 end
