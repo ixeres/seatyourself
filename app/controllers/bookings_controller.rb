024d58a56c483d7aa8e27951fc4f14eadee1bd53
Sam Slotnick <samslotnick@Sams-MacBook-Pro.local>
2017-04-24 02:31:08 -0400

    started controller for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,35 @@
 class BookingsController < ApplicationController
-  def index
-  end
 
+  def take_guest
+    Restaurant
+  end
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
-  def edit
+  def create
+
+    #below 'unless' is redundant or should be rewritten maybe, given the bookings model
+    unless restaurant.guests =< 0
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+        redirect_to booking_url
+    Restaurant.update_attribute(:numericality => -1)
+
+    else
+      flash[:alert] = "Restaurant is full"
+    end
+    end
   end
+
+  private
+  def booking_params
+    params.require(:restaurant, :guests, :date, :time)
+  end
+
 end
